9844729a3074a45a989f9658433257f63dcfe330
dimitrieh <dimitriehoekstra@gmail.com>
2016-12-20 01:32:29 +0100

    fixed minor install mistake

--- system/install.sh
+++ system/install.sh
@@ -1,6 +1,8 @@
 echo "Checking if Projects folder already exists"
-if [ ! -d "$DIRECTORY" ]; then
+if [ ! -d ~/Projects ]; then
   echo "Projects folder does not yet exists, creating..."
   mkdir ~/Projects
   echo "Projects folder created at ~/Projects"
+else
+  echo "Projects folder already exists"
 fi
