12a12348631d7c4e3309e5329a35417095f6ad57
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2017-04-05 09:41:22 +0200

    added a gitlab gdk alias to install all dependencies and then run the gdk

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -6,3 +6,5 @@ alias gdkrunner="gitlab-ci-multi-runner"
 alias gltodos="chromeless https://gitlab.com/dashboard/todos"
 alias glissues="chromeless https://gitlab.com/dashboard/issues?assignee_id=489558"
 alias glmrs="chomeless https://gitlab.com/dashboard/merge_requests?assignee_id=489558"
+
+alias nbigdkrun="npm install && bundle install && gdk run"
