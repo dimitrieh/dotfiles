ee135dbe1aa74ad29197bae2dc4f95f6f9bebd18
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2017-07-12 22:54:56 +0200

    add docker delete all containers and images aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,4 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
+alias ddelc='docker rm $(docker ps -a -q)' # Delete all containers
+alias ddeli='docker rmi $(docker images -q)' # Delete all images
