1e9679994c90c695a2b6b2ab6e60792d50059862
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2017-09-16 00:23:34 +0200

    added information on brew commands in brew alias file

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,1 +1,25 @@
+# ### Dump
+#
+# You can create a `Brewfile` from all the existing Homebrew packages you have installed with:
+#
+#     $ brew bundle dump
+#
+# The `--force` option will allow an existing `Brewfile` to be overwritten as well.
+#
+# ### Cleanup
+#
+# You can also use `Brewfile` as a whitelist. It's useful for maintainers/testers who regularly install lots of formulae. To uninstall all Homebrew formulae not listed in `Brewfile`:
+#
+#     $ brew bundle cleanup
+#
+# Unless the `--force` option is passed, formulae will be listed rather than actually uninstalled.
+#
+# ### Check
+#
+# You can check there's anything to install/upgrade in the `Brewfile` by running:
+#
+#     $ brew bundle check
+#
+# This provides a successful exit code if everything is up-to-date so is useful for scripting.
+
 alias bcask='brew cask'
