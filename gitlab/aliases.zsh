39ea41c5eded63cc99a18441ee5a5ea35de09e6d
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2017-10-02 12:34:20 +0200

    added and moved gitlab gdk aliases for quicker migrating

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -1,5 +1,8 @@
+alias migrategl='bin/rake db:migrate RAILS_ENV=development'
 alias gdkce="cd ~/Projects/gdk-ce/gitlab"
+alias gdkcemg="gdkce && migrategl && o http://localhost:3000"
 alias gdkee="cd ~/Projects/gdk-ee/gitlab"
+alias gdkeemg="gdkee && migrategl && o http://localhost:3001"
 alias gldes="cd ~/Projects/gitlab-design"
 alias gdkrunner="gitlab-ci-multi-runner"
 
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,2 +1,1 @@
-alias migrategl='bin/rake db:migrate RAILS_ENV=development'
 alias be='bundle exec'
