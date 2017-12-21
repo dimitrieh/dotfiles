c079bba3d9de8886bac3b2bdf78de4e84d576f1b
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2017-12-21 20:36:49 +0100

    Making killing of taken localhost processes easier, plus fixing alias typo

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -17,7 +17,7 @@ alias gltodos="chromeless https://gitlab.com/dashboard/todos"
 alias glissues="chromeless https://gitlab.com/dashboard/issues?assignee_id=489558"
 alias glmrs="chomeless https://gitlab.com/dashboard/merge_requests?assignee_id=489558"
 
-alias cp3000='lsof | grep ":3000"''
-alias cp3001='lsof | grep ":3001"''
+alias cp3000='lsof | grep ":3000"'
+alias cp3001='lsof | grep ":3001"'
 
-alias kill9="kill -9"
+alias kill9="kill -9" #kill -9 ID found with lsof
