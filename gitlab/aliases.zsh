02849227fcc7ecc4dce9932a5e3b67ef3a45d4d4
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2017-12-21 14:17:07 +0100

    added automatisation aliases for starting gdk's

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -1,16 +1,21 @@
 alias migrategl='bin/rake db:migrate RAILS_ENV=development'
+
 alias gdkce="cd ~/Projects/gdk-ce/gitlab"
 alias gdkcemg="gdkce && migrategl && o http://localhost:3000"
+alias gdkcerun="gdkce && npm install && bundle install && gdk run & (sleep 120 && gdkcemg)"
+
 alias gdkee="cd ~/Projects/gdk-ee/gitlab"
 alias gdkeemg="gdkee && migrategl && o http://localhost:3001"
+alias gdkeerun="gdkee && npm install && bundle install && gdk run & (sleep 120 && gdkeemg)"
+
 alias gldes="cd ~/Projects/gitlab-design"
+
 alias gdkrunner="gitlab-ci-multi-runner"
+
 
 alias gltodos="chromeless https://gitlab.com/dashboard/todos"
 alias glissues="chromeless https://gitlab.com/dashboard/issues?assignee_id=489558"
 alias glmrs="chomeless https://gitlab.com/dashboard/merge_requests?assignee_id=489558"
-
-alias nbigdkrun="npm install && bundle install && gdk run"
 
 alias cp3000='lsof | grep ":3000"''
 alias cp3001='lsof | grep ":3001"''
