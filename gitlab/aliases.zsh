4ba5f1b955b65ecda63d94167beb8e495db40a3f
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-01-07 00:04:22 +0100

    added a kill lingering processes command for gitlab gdk

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -22,4 +22,4 @@ alias cp3001='lsof | grep ":3001"'
 
 alias kill9="kill -9" #kill -9 ID found with lsof
 
-alias gdkkill="killall chromedriver chromium-browser ruby ruby2.3 node postgres" #kill all lingering processes, if not working use `-9`
+alias gdkkill="killall chromedriver chromium-browser ruby ruby2.3 node postgres" #kill all lingering processes, if not working use -9
