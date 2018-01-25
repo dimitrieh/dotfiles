6a0d6fddf628d56161e94dcc78e8cfde269432c7
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-01-25 12:52:33 +0100

    deprecate gitlab-ci-multi-runner alias due to new command being used for gitlab runners

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -10,9 +10,6 @@ alias gdkeerun="gdkee && npm install && bundle install && gdk run & (sleep 120 &
 
 alias gldes="cd ~/Projects/gitlab-design"
 
-alias gdkrunner="gitlab-ci-multi-runner"
-
-
 alias gltodos="chromeless https://gitlab.com/dashboard/todos"
 alias glissues="chromeless https://gitlab.com/dashboard/issues?assignee_id=489558"
 alias glmrs="chomeless https://gitlab.com/dashboard/merge_requests?assignee_id=489558"
