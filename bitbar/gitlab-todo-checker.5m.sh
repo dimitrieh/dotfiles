1e0c8066c5b6704dfa7600fca9caf656588989cc
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2018-01-29 18:43:16 +0100

    add todos with label vision section

--- bitbar/gitlab-todo-checker.5m.sh
+++ bitbar/gitlab-todo-checker.5m.sh
@@ -63,6 +63,24 @@ $(printf %-75.75s "$([[ $state == *'opened'* ]] && echo '' || echo "("$state") "
 done < <(/usr/local/bin/jq -rc '.[] | select(.project.path == "gitlab-design") | .target.iid,.project.path,.target.state,.target.labels,.target.title,.target_url' < /tmp/gitlab-todo-checker-1-1.json);
 
 echo "---";
+echo "Todo's with label vision";
+while read -r iid
+      read -r path
+      read -r state
+      read -r labels
+      read -r title
+      read -r target_url; do
+    echo "\
+$(printf %-15.15s "$path")\
+$([[ $target_url == *'merge_requests'* ]] && echo '!' || echo '#')\
+$(printf '%-6s' "$iid")\
+$(printf '%-2.2s' "$(echo ${labels} | /usr/local/bin/jq '.[]? | select(. == "'$speciallabel'")' | sed 's/"//g' | sed 's/^\(.\).*/\1/')")\
+$(printf '%-2.2s' "$(echo ${labels} | /usr/local/bin/jq '.[]? | select(. == "'$speciallabel2'")' | sed 's/"//g' | sed 's/^\(.\).*/\1/')")\
+$(printf %-75.75s "$([[ $state == *'opened'* ]] && echo '' || echo "("$state") ")$title")\
+| href=$target_url font=$monofont"
+done < <(/usr/local/bin/jq -rc '.[] | select(.target.labels[]? == "vision") | .target.iid,.project.path,.target.state,.target.labels,.target.title,.target_url' < /tmp/gitlab-todo-checker-1-1.json);
+
+echo "---";
 echo "Todo's on merge requests";
 while read -r iid
       read -r path
