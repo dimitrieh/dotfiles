e452c5080b4b4c57dfd702a211d767b46a446332
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2020-10-23 14:10:21 +0200

    Added assignment checker lists for P1, P2, and Standby issue labels instead of direct assigned issues

--- bitbar/gitlab-assignments-checker.20m.sh
+++ bitbar/gitlab-assignments-checker.20m.sh
@@ -24,14 +24,35 @@ for i in $(seq 1 $PAGES); do
   curl -s -L -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/merge_requests?scope=all&assignee_id=$assigneeid&state=opened&per_page=100&page=$i" >> /tmp/gitlab-assignments-checker-2-1.json;
 done
 
-# Getting all assignments awarded a star emoji by you
-> /tmp/gitlab-assignments-checker-3-1.json
-PAGES=$(curl -i -s -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&assignee_id=$assigneeid&my_reaction_emoji=star&state=opened&per_page=100" | grep -Fi X-Total-Pages | awk '/X-Total-Pages/ { print $2 }' | tr -d '\r');
+# # Getting all assignments awarded a star emoji by you
+# > /tmp/gitlab-assignments-checker-3-1.json
+# PAGES=$(curl -i -s -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&assignee_id=$assigneeid&my_reaction_emoji=star&state=opened&per_page=100" | grep -Fi X-Total-Pages | awk '/X-Total-Pages/ { print $2 }' | tr -d '\r');
+# for i in $(seq 1 $PAGES); do
+#   curl -s -L -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&assignee_id=$assigneeid&my_reaction_emoji=star&state=opened&per_page=100&page=$i" >> /tmp/gitlab-assignments-checker-3-1.json;
+# done
+
+# p1
+> /tmp/gitlab-assignments-checker-4-1.json
+PAGES=$(curl -i -s -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&labels=Dimi%3A%3AIn%20Progress%20P1&state=opened&per_page=100" | grep -Fi X-Total-Pages | awk '/X-Total-Pages/ { print $2 }' | tr -d '\r');
 for i in $(seq 1 $PAGES); do
-  curl -s -L -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&assignee_id=$assigneeid&my_reaction_emoji=star&state=opened&per_page=100&page=$i" >> /tmp/gitlab-assignments-checker-3-1.json;
+  curl -s -L -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&labels=Dimi%3A%3AIn%20Progress%20P1&state=opened&per_page=100&page=$i" >> /tmp/gitlab-assignments-checker-4-1.json;
 done
 
-cat /tmp/gitlab-assignments-checker-1-1.json > /tmp/gitlab-assignments-checker-4-1.json
+# p2
+> /tmp/gitlab-assignments-checker-5-1.json
+PAGES=$(curl -i -s -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&labels=Dimi%3A%3AIn%20Progress%20P2&state=opened&per_page=100" | grep -Fi X-Total-Pages | awk '/X-Total-Pages/ { print $2 }' | tr -d '\r');
+for i in $(seq 1 $PAGES); do
+  curl -s -L -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&labels=Dimi%3A%3AIn%20Progress%20P2&state=opened&per_page=100&page=$i" >> /tmp/gitlab-assignments-checker-5-1.json;
+done
+
+# standby
+> /tmp/gitlab-assignments-checker-6-1.json
+PAGES=$(curl -i -s -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&labels=Dimi%3A%3AStandby&state=opened&per_page=100" | grep -Fi X-Total-Pages | awk '/X-Total-Pages/ { print $2 }' | tr -d '\r');
+for i in $(seq 1 $PAGES); do
+  curl -s -L -H "PRIVATE-TOKEN: $privatetoken" "https://gitlab.com/api/v4/issues?scope=all&labels=Dimi%3A%3AStandby&state=opened&per_page=100&page=$i" >> /tmp/gitlab-assignments-checker-6-1.json;
+done
+
+# cat /tmp/gitlab-assignments-checker-1-1.json > /tmp/gitlab-assignments-checker-4-1.json
 
 # Creating count for assigned mr's, all assigned issues
 number1=$(cat /tmp/gitlab-assignments-checker-2-1.json | jq -rc '.[] | .web_url' | wc -l | tr -d '[:space:]')
@@ -61,6 +82,58 @@ $(printf %-75.75s "$title")\
 | href=$web_url font=$monofont size=$monosize";
 echo $web_url >> $file;
 done < <(jq -rc '.[] | .iid,.labels,.title,.web_url' < /tmp/gitlab-assignments-checker-2-1.json);
+
+echo "---";
+file=/tmp/gitlab-assignments-checker-1-p1.txt
+> $file
+echo "Dimi P1 | bash=/Users/dimitrie/.dotfiles/bin/openlist param1=$file terminal=false color=$headercolor";
+while read -r iid
+      read -r labels
+      read -r title
+      read -r web_url; do
+    echo "\
+$(printf %-15.15s "$(echo $web_url | sed -E 's#([^/]+)/(issues|merge_requests)/[0-9]+#\1#' | sed -E 's#.*/([^/]+)#\1#')") $([[ $web_url == *'merge_requests'* ]] && echo '!' || echo '#')\
+$(printf '%-6s' "$iid")\
+$(printf '%-2.2s' "$(echo ${labels} | jq '.[]? | select(. == "'$speciallabel'")' | sed 's/"//g' | sed 's/^\(.\).*/\1/')")\
+$(printf %-75.75s "$title")\
+| href=$web_url font=$monofont size=$monosize";
+echo $web_url >> $file;
+done < <(jq -rc '.[] | .iid,.labels,.title,.web_url' < /tmp/gitlab-assignments-checker-4-1.json);
+
+echo "---";
+file=/tmp/gitlab-assignments-checker-1-p2.txt
+> $file
+echo "Dimi P2 | bash=/Users/dimitrie/.dotfiles/bin/openlist param1=$file terminal=false color=$headercolor";
+while read -r iid
+      read -r labels
+      read -r title
+      read -r web_url; do
+    echo "\
+$(printf %-15.15s "$(echo $web_url | sed -E 's#([^/]+)/(issues|merge_requests)/[0-9]+#\1#' | sed -E 's#.*/([^/]+)#\1#')") $([[ $web_url == *'merge_requests'* ]] && echo '!' || echo '#')\
+$(printf '%-6s' "$iid")\
+$(printf '%-2.2s' "$(echo ${labels} | jq '.[]? | select(. == "'$speciallabel'")' | sed 's/"//g' | sed 's/^\(.\).*/\1/')")\
+$(printf %-75.75s "$title")\
+| href=$web_url font=$monofont size=$monosize";
+echo $web_url >> $file;
+done < <(jq -rc '.[] | .iid,.labels,.title,.web_url' < /tmp/gitlab-assignments-checker-5-1.json);
+
+echo "---";
+file=/tmp/gitlab-assignments-checker-1-standby.txt
+> $file
+echo "Dimi Standby | bash=/Users/dimitrie/.dotfiles/bin/openlist param1=$file terminal=false color=$headercolor";
+while read -r iid
+      read -r labels
+      read -r title
+      read -r web_url; do
+    echo "\
+$(printf %-15.15s "$(echo $web_url | sed -E 's#([^/]+)/(issues|merge_requests)/[0-9]+#\1#' | sed -E 's#.*/([^/]+)#\1#')") $([[ $web_url == *'merge_requests'* ]] && echo '!' || echo '#')\
+$(printf '%-6s' "$iid")\
+$(printf '%-2.2s' "$(echo ${labels} | jq '.[]? | select(. == "'$speciallabel'")' | sed 's/"//g' | sed 's/^\(.\).*/\1/')")\
+$(printf %-75.75s "$title")\
+| href=$web_url font=$monofont size=$monosize";
+echo $web_url >> $file;
+done < <(jq -rc '.[] | .iid,.labels,.title,.web_url' < /tmp/gitlab-assignments-checker-6-1.json);
+
 
 # echo "---";
 # file=/tmp/gitlab-assignments-checker-1-workflow-problem-validation.txt
