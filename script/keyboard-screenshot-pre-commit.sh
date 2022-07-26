97887bb348d6792faec11839cbb314721b333935
Dimitrie Hoekstra <dimitriehoekstra@gmail.com>
2022-07-26 19:35:20 +0200

    Double the DPI of keyboard png render

--- script/keyboard-screenshot-pre-commit.sh
+++ script/keyboard-screenshot-pre-commit.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # Create a screenshot of keyboard.html
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --force-device-scale-factor=1 --disable-gpu --screenshot=keyboard.png --window-size=1100,516 keyboard.html
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --force-device-scale-factor=1 --disable-gpu --screenshot=keyboard.png --window-size=2200,1032 keyboard.html
+convert -units PixelsPerInch keyboard.png -density 144 keyboard.png
