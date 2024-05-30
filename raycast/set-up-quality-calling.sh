6ea89b4351edaf67a0cdbc1c7010b60962702454
Dimitrie Hoekstra <hi@dimitr.ie>
2024-05-30 13:37:46 +0200

    Define command set sound input output devices

---
+++ raycast/set-up-quality-calling.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Set Up Quality Calling
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Set correct sound input and output devices
+# @raycast.author dimitrieh
+# @raycast.authorURL https://raycast.com/dimitrieh
+
+qualitycalling
