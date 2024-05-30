4ec3e5ceef9d8aecbc1c57d0c277d65389b986f5
Dimitrie Hoekstra <hi@dimitr.ie>
2024-05-30 14:28:41 +0200

    Changed script to raycast script

--- raycast/set-up-quality-calling.sh
+++ raycast/set-up-quality-calling.sh
@@ -13,4 +13,17 @@
 # @raycast.author dimitrieh
 # @raycast.authorURL https://raycast.com/dimitrieh
 
-qualitycalling
+osascript <<END
+if application "Rode Connect" is running then
+    return "RUNNING"
+else
+    tell application "Rode Connect" to activate
+    delay 10
+end if
+END
+if SwitchAudioSource -t output -u 00-C5-85-6C-08-8C:output && SwitchAudioSource -t input -u RodeConnectAudioDevice_UID ; then
+    echo "Command succeeded"
+else
+    osascript -e 'display notification "Ideal sound devices could not be connected. Connect them and try again." with title "Failed to connect ideal sound devices" sound name "Sosumi"'
+fi
+osascript -e 'display notification "Ideal sound devices Connected" sound name "Blow"'
