a71017d7c149a8efb36cc4cd48ff46057071bc96
Dimitrie Hoekstra <hi@dimitr.ie>
2025-08-22 15:18:56 +0200

    Add xbar menu bar plugins
    - Add SSH status monitoring plugin
    - Add Mac Mini metrics display plugin

---
+++ xbar/plugins/001-ssh-status.10min.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# <xbar.title>SSH Status</xbar.title>
+# <xbar.version>v0.1.0</xbar.version>
+# <xbar.author>Olivier Tille</xbar.author>
+# <xbar.author.github>oliviernt</xbar.author.github>
+# <xbar.image>http://i.imgur.com/s8FMCLG.png</xbar.image>
+# <xbar.desc>Continuously checks if hosts are available for ssh connection</xbar.desc>
+# <xbar.dependencies>Bash</xbar.dependencies>
+#
+# SSH Status plugin
+# by Olivier Tille (@oliviernt)
+#
+# Continuously checks if hosts are available for ssh connection on port 22
+HOSTS=("dimitries-mac-mini")
+head="ssh status"
+body=""
+errors=0
+for host in "${HOSTS[@]}"; do
+  if nc -z -G 2 "$host" 22 &> /dev/null; then
+    body="$body\n🟢 $host is up | bash=ssh param1=$host"
+  else
+    body="$body\n🔻 $host looks down from here | bash=ssh param1=$host"
+    errors=$((errors + 1))
+  fi
+  body="$body\n---"
+done
+
+echo -n "$head"
+[ "$errors" -eq 0 ] || echo -n " ($((${#HOSTS[@]} - errors))/${#HOSTS[@]}) | color=red"
+echo -e "\n---"
+echo -e "$body"
